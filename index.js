40cd1f2d54848a31bed2d5de2cbe8aec251631e5
Austin Rognes <acrognes@iastate.edu>
2019-12-29 22:11:39 -0600

    Fullscreen toggle added

--- index.js
+++ index.js
@@ -373,3 +373,15 @@ function draw() {
     steps.push(step);
     requestAnimationFrame(draw);
  }
+
+ // call fullscreen methods compatible for all browsers, retrieved from: https://developers.google.com/web/fundamentals/native-hardware/fullscreen/
+ function toggleFullscreen() {
+    var doc = window.document;
+    var docEl = doc.documentElement;
+  
+    var requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen;
+    var cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen;
+  
+    if(!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) requestFullScreen.call(docEl);
+    else cancelFullScreen.call(doc);
+  }
