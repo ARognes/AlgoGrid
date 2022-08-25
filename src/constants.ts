d8b12aad219490265f5b94ee6133863e17f7088a
ARognes <aurognes@gmail.com>
2022-08-24 20:34:37 -0500

    Checkpoint

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+
+export const GRID_LINE_WIDTH = 6,
+             MIN_WIDTH = 2,
+             MAX_WIDTH = 256,
+             SHOW_LABEL_DIST = 6
+
+export const TILE_SIZE = 32
+
+export const SQRT_2 = Math.sqrt(2)
